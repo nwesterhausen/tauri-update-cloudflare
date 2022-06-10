f44bcc3bdd2dba6d7a4005f45bbc9096101bf3a6
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2022-06-10 12:03:44 -0400

    fix: 💄 add more whitespace around headings and don't include the level

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -2,47 +2,47 @@ import { marked } from 'marked';
 
 // &#63; to ? helper
 const htmlEscapeToText = (text: string) => {
-  return text.replace(/&#[0-9]*;|&amp;/g, (escapeCode) => {
-    if (escapeCode.match(/amp/)) {
-      return '&';
-    }
-    const match = escapeCode.match(/[0-9]+/);
-    if (match !== null) {
-      return String.fromCharCode(parseInt(match[0]));
-    }
-    return '';
-  });
+    return text.replace(/&#[0-9]*;|&amp;/g, (escapeCode) => {
+        if (escapeCode.match(/amp/)) {
+            return '&';
+        }
+        const match = escapeCode.match(/[0-9]+/);
+        if (match !== null) {
+            return String.fromCharCode(parseInt(match[0]));
+        }
+        return '';
+    });
 };
 const renderPlain = () => {
-  const render = new marked.Renderer();
-  // render just the text of a link
-  render.link = function (href, title, text) {
-    return text;
-  };
-  // render just the text of a paragraph
-  render.paragraph = function (text) {
-    return htmlEscapeToText(text) + '\r\n';
-  };
-  // render just the text of a heading element, but indecate level
-  render.heading = function (text, level) {
-    return level + ' ) ' + text;
-  };
-  // render nothing for images
-  render.image = function (href, title, text) {
-    return '';
-  };
-  // render lists?
-  render.list = function (text, ordered, start) {
-    return '';
-  }
-  render.listitem = function(text, task, checked) {
-    return `   ${text}\r\n`;
-  }
-  return render;
+    const render = new marked.Renderer();
+    // render just the text of a link
+    render.link = function (href, title, text) {
+        return text;
+    };
+    // render just the text of a paragraph
+    render.paragraph = function (text) {
+        return htmlEscapeToText(text) + '\r\n';
+    };
+    // render just the text of a heading element
+    render.heading = function (text, level) {
+        return `\r\n${text}\r\n`;
+    };
+    // render nothing for images
+    render.image = function (href, title, text) {
+        return '';
+    };
+    // render lists?
+    render.list = function (text, ordered, start) {
+        return '';
+    }
+    render.listitem = function (text, task, checked) {
+        return `   ${text}\r\n`;
+    }
+    return render;
 };
 
 export const MarkdownToPlain = (markdown: string): string => {
-  return marked(markdown, {
-    renderer: renderPlain(),
-  });
+    return marked(markdown, {
+        renderer: renderPlain(),
+    });
 };
